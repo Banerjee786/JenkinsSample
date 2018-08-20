0d01098b27b7aed9296ba8b34e721957885a5eca
Banerjee786 <priyambanerjee08@gmail.com>
2018-08-20 13:48:54 -0400

    Added HelloWorld Jenkins program

---
+++ hello.java
@@ -0,0 +1,8 @@
+class hello
+{
+    public static void main(String args[]){
+        for(int i=0;i<10;i++){
+            System.out.println(i+" Welcome to Jenkins Sample Job Run");
+        }
+    }
+}   
